e417e0944de3f618b7b25e24b745a3fcd4b0bb21
Mirella Lins <mirella.lds@gmail.com>
2017-04-08 21:36:15 -0300

    Result screen

--- app/src/main/java/br/com/teampenha/quizsocial/ResultActivity.java
+++ app/src/main/java/br/com/teampenha/quizsocial/ResultActivity.java
@@ -2,12 +2,29 @@ package br.com.teampenha.quizsocial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import br.com.teampenha.quizsocial.presenter.ResultPresenter;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView txtPoints, txtCompartilhar, txtDesafio, txtJogarNovamente;
+    private ResultPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        this.ini();
+
+        txtPoints.setText(mPresenter.getPoint());
+    }
+    public void ini() {
+        txtPoints = (TextView) findViewById(R.id.content_points);
+        txtCompartilhar = (TextView) findViewById(R.id.compartilhar);
+        txtDesafio = (TextView) findViewById(R.id.desafiar);
+        txtJogarNovamente = (TextView) findViewById(R.id.jogar_novamente);
+
+        mPresenter = new ResultPresenter();
     }
 }
